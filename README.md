b7c2e5b2ffe6f3b70de52fee89b9fa23c27494f4
andrew <statco19@gmail.com>
2022-11-11 23:37:07 +0900

    implemented launch screen

---
+++ README.md
@@ -0,0 +1,3 @@
+# zzzapp
+
+new~~
---
+++ ZalZaZa/ZalZaZa/APP/ZalZaZaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZalZaZaApp.swift
+//  ZalZaZa
+//
+//  Created by 정은서 on 2022/11/09.
+//
+
+import SwiftUI
+
+@main
+struct ZalZaZaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
